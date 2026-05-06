94a13428aeaff6b0e65d4f56960cccd2ec29a4de
warisakn <warisakn@gmail.com>
2026-05-07 00:57:59 +0200

    Add seeders

---
+++ database/seeders/ChirpSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ChirpSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = User::all();
+
+        // Create chirps for random users
+        for ($i = 0; $i < 3; $i++) 
+        {
+            $users->random()->chirps()->create([
+                'message' => fake()->text(),
+                'created_at' => now()->subMinutes(rand(5, 1440)),
+            ]);
+        }
+    }
+}
